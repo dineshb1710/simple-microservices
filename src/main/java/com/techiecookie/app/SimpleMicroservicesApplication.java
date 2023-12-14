c2da5f7e8649ef94abbb0880452ed3609937320a
Dinesh Kumar Bhardwaj <dinesh.bhardwaj1710@gmail.com>
2023-12-14 13:42:22 +0530

    Initial Commit

---
+++ src/main/java/com/techiecookie/app/SimpleMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.techiecookie.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMicroservicesApplication.class, args);
+	}
+
+}
